039b9412edc96010c710bbad7b8d4a7e90a00d08
Mark-ThinkPad <1372469698@qq.com>
2020-05-27 19:04:55 +0800

    new web front-end UI

--- views.py
+++ views.py
@@ -6,3 +6,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def index():
     return render_template('base.html')
+
+
+@views.route('/user/login')
+def user_login():
+    return '用户登录'
+
+
+@views.route('/user/add')
+def add_user():
+    return '用户注册'
+
+
+@views.route('/notes')
+def get_notes():
+    return '云笔记'
+
+
+@views.route('/notes/add')
+def add_notes():
+    return '添加云笔记'
