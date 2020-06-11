652d6a6e358a4b4fa934f12ac88d85f7fcb6d5f3
Mark-ThinkPad <1372469698@qq.com>
2020-06-11 22:05:54 +0800

    new web front-end and backend solution

--- conf/config.py
+++ conf/config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from conf.settings import DATABASE_DIR
 
 
@@ -5,5 +6,6 @@ class Config:
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_DIR + '/models.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # session设置
+    # session配置
     SECRET_KEY = b'\x07\xb0\xa8wwGp\xb0l\x90\xfd|C!t\xf8'
+    PERMANENT_SESSION_LIFETIME = timedelta(days=3)
