bf28aeda63f9d6378af858175e72af224e8b4c2a
Mark-ThinkPad <1372469698@qq.com>
2020-06-12 17:39:12 +0800

    new web front-end and backend solution

--- app/api.py
+++ app/api.py
@@ -3,6 +3,17 @@ from flask import Blueprint, request
 api = Blueprint('api', __name__)
 
 
+@api.route('/user/add', methods=['POST'])
+def user_add():
+    username = request.form.get('username', False)
+    password = request.form.get('password', False)
+
+    if not (username and password):
+        return {'success': False, 'message': '传入数据不全'}
+
+    return {'success': True, 'message': 'API在线'}
+
+
 @api.route('/test', methods=['POST'])
 def test():
     return {'TestAPI': False}
--- static/js/user_add.js
+++ static/js/user_add.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
+  $id('user_add').addEventListener('click', function () {
+    let $username = $id('username').value;
+    let $password = $id('password').value;
+    if ($username.length === 0 || $password.length === 0) {
+      alert('请输入完整的用户名和密码');
+    } else {
+      ajax({
+        url: '/api/user/add',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          username: $username,
+          password: $password
+        },
+        success: function (response) {
+          console.log(response, typeof(response.success));
+        },
+        fail: function (status) {
+          console.log(status)
+        }
+      });
+    }
+  });
 });
